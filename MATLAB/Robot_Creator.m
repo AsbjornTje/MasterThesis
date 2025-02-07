89cd69cd57271964c0c0ce668f10b484843ab1b7
AsbjornTje <115251369+AsbjornTje@users.noreply.github.com>
2025-02-07 11:55:16 +0100

    Fixed DH parameters

--- MATLAB/Robot_Creator.m
+++ MATLAB/Robot_Creator.m
@@ -2,19 +2,33 @@ clear all;
 clc;
 %close all;
 
+q = [0 -pi/2 0 pi/2 0 0 0 0]
+
 % Specify Dh parameters
 dhparams = [
-    %a(length along x)     alpha(rotation around x)    d(length along Z)      theta(rotation around z) 
-     0       -pi    0.4    0;         
-     0.2     0.5*pi   0.3    0;
-     0        -0.5*pi   1     0;
-     0.1          0       0.5    0
-];
+    %a(t_x)   alpha(r_x)   d(t_z)    theta(r_z) 
+    0.000     -pi/2        -0.0745   0.000;      % shoulder yaw   
+    0.000     -pi/2        -0.183    0.000;      % shoulder pitch
+    0.000      0.000        0.500   -pi/2;       % fixed prismatic link
+    0.000      0.000        1.000    0.000;      % shoulder prismatic joint
+    0.100      0.000        0.000    0.000;      % fixed prismatic link offset
+    0.000      0.000        0.100    0.000;      % first yaw joint new arm
+    0.000      pi/2         0.300    0.000;      % l1
+    0.000      0.000        0.100    0.000;      % first pitch joint
+    0.000     -pi/2         0.000    0.000;      % l2
+    0.000     0.000         0.100    0.000;      % second yaw joint
+    0.000     pi/2          0.300    0.000;      % l3
+    0.000     0.000         0.100    0.000;      % second pitch joint
+    0.000     -pi/2         0.000    0.000;      % l4
+    0.000     0.000         0.100    0.000;      % third yaw joint
+    0.000     0.000         0.200    0.000;      % End effector
+    ];
 
 % Specify joint types
 rev = "revolute";
 pris = "prismatic";
-jointTypes = [rev, rev, pris, rev];
+fix = "fixed";
+jointTypes = [rev, rev,fix ,pris, fix, rev,fix, rev, fix, rev, fix, rev, fix, rev, fix, rev];
 
 % Specify link radius
 r = 0.07;
@@ -46,41 +60,32 @@ for i = 1:numJoints
     else % Add current body to previous body by name
         addBody(robot, bodies{i}, bodies{i-1}.Name);
     end
-
-     if jointTypes(i) == "revolute"
-        % Cylinder for revolute joints
-        if dhparams(i,3) ~= 0  % If 'd' (length along z) is nonzero, use it for height
-            h = abs(dhparams(i,3)); 
-        elseif dhparams(i,1) ~= 0  % Otherwise, use 'a' (length along x)
-            h = abs(dhparams(i,1));
-        else
-            h = 0.1; % Default small cylinder
-        end
-        collisionObj = collisionCylinder(r, h);
-        
-        % Positioning: Shift cylinder along the correct axis
-        if dhparams(i,3) ~= 0
-            T = trvec2tform([0, 0, h/2]); % Shift along z
-        elseif dhparams(i,1) ~= 0
-            T = trvec2tform([h/2, 0, 0]); % Shift along x
-        else
-            T = eye(4);
-        end
-    
-    elseif jointTypes(i) == "prismatic"
-        % Box for prismatic joints (to represent sliding)
-        boxLength = max(abs(dhparams(i,3)), 0.3); % Ensure a reasonable box size
-        boxWidth = 0.1; % Fixed width
-        boxHeight = 0.1; % Fixed height
-        collisionObj = collisionBox(boxLength, boxWidth, boxHeight);
-        
-        % Positioning: Shift box along the Z-axis to match prismatic motion
-        T = trvec2tform([0, 0, boxLength / 2]);
-    end
-    
-    % Add collision object with transformation
-    addCollision(robot.Bodies{i}, collisionObj, T);
 end
+
+
+%Perform forward kinematics
+
+% Specify home position for joints
+jnt2.HomePosition = pi/2
+
+
+% Display the details of the constructed manipulator for verification
+disp('Constructed a rigidBodyTree from DH parameters:');
+showdetails(robot);
+
+% Display the manipulator in 3d space
+%gui = interactiveRigidBodyTree(robot,"Configuration",q,"MarkerScaleFactor",0.5)
+show(robot,q)
+
+
+% disp("Press any key to close")
+% pause('on');
+%     pause;
+%close All;
+
+
+
+
 
 %     % Adjust the collision object size dynamically
 %     % Use 'a' (link length along x) or 'd' (link length along z) to determine height
@@ -108,17 +113,3 @@ end
 %     % Add the collision object with transformation
 %     addCollision(robot.Bodies{i}, collisionObj, T);
 % end
-
-% Display the details of the constructed manipulator for verification
-disp('Constructed a rigidBodyTree from DH parameters:');
-showdetails(robot);
-
-% Display the manipulator in 3d space
-figure(Name="Manipulator plot");
-show(robot,Collisions="on");
-
-disp("Press any key to close")
-pause('on');
-    pause;
-close All;
-
